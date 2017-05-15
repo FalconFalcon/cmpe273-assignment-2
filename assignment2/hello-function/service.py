de71b26f272615cb64c31815e4dca6ec8e180c42
Adithya KLN <adithyakarlapalem@gmail.com>
2017-05-15 10:49:36 -0700

    Added method to handle POST request.

--- assignment2/hello-function/service.py
+++ assignment2/hello-function/service.py
@@ -2,10 +2,29 @@
 
 print "Loading function..."
 
-def handler(event, context):
-    print "Hello World"
-    print event
-    # Your code goes here!
-    #e = event.get('e')
-    #pi = event.get('pi')
-    return "OK"
+import boto3
+
+
+def lambda_handler(event, context):
+    print("Hello World")
+    dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+    menuDB = dynamodb.Table('PizzaMenu')
+
+    httpMethod = event.get('httpMethod')
+    data = event.get('data')
+
+    if httpMethod == "POST":
+        menuDB.put_item(
+            Item={
+                "menu_id": data["menu_id"],
+                "store_name": data["store_name"],
+                "selection": data["selection"],
+                "size": data["size"],
+                "price": data["price"],
+                "store_hours": data["store_hours"]
+            }
+        )
+
+        return {
+            "status": 200
+        }
